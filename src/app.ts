9677195c6bc19eda08834f0583c74ec084180408
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-25 17:15:05 -0300

    bloqueando chamadas que nao sejam do getaway

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
 import studentsRouter from "./routes/studentsRouter";
-import cors from "cors";
+import cors, { CorsOptions } from "cors"; // Middleware para habilitar CORS
 import professionalsRouter from "./routes/professionalsRouter";
 
 dotenv.config();
 
 const app = express();
 
-// Utiliza o middleware CORS para permitir requisições de diferentes origens
-app.use(
-  cors({
-    origin: ["http://localhost:4000"],
-  })
-);
-
-app.use((req, res) => {
-  res.status(404);
-});
-
 const port = process.env.PORT || 3000;
-app.use(cors());
+
+const ALLOWED_ORIGIN = "http://localhost:4000";
+
+const corsOptions: CorsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin) {
+      // Se não há cabeçalho de origem, rejeitamos a requisição
+      callback(new Error("Origin header missing or undefined"));
+      return;
+    }
+
+    if (origin === ALLOWED_ORIGIN) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
